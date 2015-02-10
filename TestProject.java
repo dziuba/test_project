930e4e70f1101b0470b8a8660b7633972847a734
dziuba <dziuba@agmk.net>
2015-02-10 10:39:17 +0100

    CleanUp

--- TestProject.java
+++
@@ -1,9 +0,0 @@
-//package dziuba.test.project;
-
-public class TestProject {
-	
-	public static void main(String[] args){
-		System.out.println("Hello World!");
-		System.out.println("Faka faka");
-	}
-}
